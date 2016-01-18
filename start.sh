48b2f5ced84eb2583e6ee78471442055db7a3982
Bahadir <bahadrix@gmail.com>
2016-01-18 16:22:49 +0200

    start-script

--- start.sh
+++ start.sh
@@ -8,8 +8,7 @@ M=20G
 m=20G
 
 d=0
-cmd="~run 9010"
-rpmCacheSize=200000
+cmd="~run"
 
 if [[ $# -eq 0 ]] ; then
     echo "Usage: load.sh [OPTS]"
@@ -17,7 +16,6 @@ if [[ $# -eq 0 ]] ; then
     echo "-t|--threads threadNum    : Thread count for SPARK. Default: $THREADS"
     echo "-mx|--max-memory maxHeap  : JVM Xmx. Default: $M"
     echo "-ms|--min-memory minHeap  : JVM Xms. Default: $m"
-    echo "-rc|--rpm-cache-size		: RPM Cache size Default: 200000"
     echo "-d|--debug port           : Debug port for remote debugging."
     echo "-c|--command              : Run command for activator. Default: $cmd"
 
@@ -25,7 +23,6 @@ if [[ $# -eq 0 ]] ; then
     exit 0
 fi
 
-acthome=${ACTIVATOR_HOME:?"Please set ACTIVATOR_HOME environment variable (eg /opt/activator-dist-1.3.5) or make the activator command accessible from everywhere. "}
 
 while [[ $# > 1 ]]
 do
@@ -48,10 +45,6 @@ do
 		d="$2"
 		shift
 		;;
-		-rc|--rpm-cache-size)
-		rpmCacheSize="$2"
-		shift
-		;;
         -c|--command)
         cmd="$2"
         shift
@@ -71,7 +64,6 @@ if [[ $d -ne 0 ]];then
 fi
 
 export SPARK_MODE="local[$THREADS]"
-export RPM_CACHE_SIZE=${rpmCacheSize}
 echo "Spark mode    	: $SPARK_MODE"
 echo "Memory        	: $m - $M"
 echo "RPM Cache Size	: $rpmCacheSize"
@@ -79,8 +71,8 @@ echo "Activator home	: $acthome"
 echo "Activator cmd 	: $cmd"
 echo "Debug Args    	: $debugArg"
 echo "--------------------------------------------"
+# java -Xms1024m -Xmx1024m -XX:ReservedCodeCacheSize=128m -XX:MaxMetaspaceSize=256m -jar /usr/share/sbt-launcher-packaging/bin/sbt-launch.jar
 
-until java -Dactivator.home=$acthome -Xms$m -Xmx$M -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled  -XX:MetaspaceSize=64m -XX:MaxMetaspaceSize=256m $debugArg -jar $acthome/activator-launch-*.jar "$cmd"; do
-    echo "WLSA Server halted with exit code $?.  Respawning in 3 seconds.." >&2
-    sleep 3
-done
+java -Xms$m -Xmx$M -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled  -XX:MetaspaceSize=64m -XX:MaxMetaspaceSize=256m $debugArg -jar /usr/share/sbt-launcher-packaging/bin/sbt-launch.jar "$cmd"; do
+
+
